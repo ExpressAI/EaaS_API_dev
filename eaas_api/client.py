c469ef37d1c9b2fced6a9cd0aafc4ef8094c08fa
yyy-Apple <50064641+yyy-Apple@users.noreply.github.com>
2021-09-06 23:56:08 +0800

    change to one-by-one scoring

--- eaas_api/client.py
+++ eaas_api/client.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 import requests
 import json
 import warnings
+from tqdm import tqdm
 
 
 class Client:
@@ -18,8 +19,8 @@ class Client:
             "comet",
             "comet_qe",
             "mover_score",
-            "prism",
-            "prism_qe",
+            # "prism",
+            # "prism_qe",
             "rouge"
         ]
         pass
@@ -30,19 +31,21 @@ class Client:
     def score(self, inputs: List[Dict], metrics=None):
         if metrics is None:
             metrics = self.valid_metrics
+            warnings.warn("You didn't specify the metrics, will use all valid metrics by default.")
         else:
             for metric in metrics:
                 assert metric in self.valid_metrics, "Your have entered invalid metric, please check."
-            warnings.warn("You didn't specify the metrics, will use all valid metrics by default.")
 
-        data = {
-            "inputs": inputs,
-            "metrics": metrics
-        }
-        response = requests.post(url=self.end_point, json=json.dumps(data))
-        rjson = response.json()
-        print(f"output: {rjson}")
-        pass
+        # Deal with the inputs one by one
+        for x in tqdm(inputs, total=len(inputs), desc="Calculating scores."):
+            data = {
+                "inputs": [x],
+                "metrics": metrics
+            }
+
+            response = requests.post(url=self.end_point, json=json.dumps(data))
+            rjson = response.json()
+            print(f"output: {rjson}")
 
     def signature(self):
         pass
