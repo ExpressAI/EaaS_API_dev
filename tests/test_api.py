4f7bb6319d764e92cebbb7017ada4c3f633de162
yyy-Apple <50064641+yyy-Apple@users.noreply.github.com>
2022-02-03 18:13:12 +0800

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
 # %%
-from eaas import Client
+from eaas import Client, Config
 import jsonlines
 from pathlib import Path
 import os
@@ -19,7 +19,8 @@ def read_jsonlines_to_list(file_name):
 class TestMetrics(unittest.TestCase):
     def test_api(self):
         client = Client()
-        client.load_config("config.json")
+        config = Config()
+        client.load_config(config)
 
         input_file = os.path.join(curr_dir, "inputs", "multi_references.jsonl")
         inputs = read_jsonlines_to_list(input_file)
@@ -28,7 +29,8 @@ class TestMetrics(unittest.TestCase):
 
     def test_multilingual(self):
         client = Client()
-        client.load_config("config.json")
+        config = Config()
+        client.load_config(config)
 
         for lang in ["en", "fr", "zh"]:
             # Single ref
@@ -36,12 +38,12 @@ class TestMetrics(unittest.TestCase):
             print("For single reference")
             input_file = os.path.join(curr_dir, "inputs", f"{lang}_single_ref_tiny.jsonl")
             inputs = read_jsonlines_to_list(input_file)
-            res = client.score(inputs, metrics=None, lang=lang)
+            res = client.score(inputs, task="sum", metrics=None, lang=lang)
             print(res)
 
             # Multi ref
             print(f"For multiple references")
             input_file = os.path.join(curr_dir, "inputs", f"{lang}_multi_ref_tiny.jsonl")
             inputs = read_jsonlines_to_list(input_file)
-            res = client.score(inputs, metrics=None, lang=lang)
+            res = client.score(inputs, task="sum", metrics=None, lang=lang)
             print(res)
